a5a8ddff0994e145a64aef2684d111ed5f3b86a7
VourdoukasD <dimvourd@gmail.com>
2024-07-03 15:36:04 +0300

    REST API CRUD Configs and Sorting
    @RepositoryRestResource(path="members")
    
    Pages
    
    sorting example: http://localhost:8080/api/members?sort=lastName

--- src/main/java/com/luv2code/springboot/cruddemo/repositories/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/repositories/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.luv2code.springboot.cruddemo.repositories;
 
 import com.luv2code.springboot.cruddemo.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
